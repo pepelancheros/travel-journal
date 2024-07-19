3add0ac0a11dfd015007510190c48767abeeaa3a
Sebastian Lancheros <sebastian.lancheros@proper.ai>
2024-07-19 13:19:05 +0200

    add a nice touch

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ export function Header() {
   return (
     <section className="header">
       <img src="src/assets/world.svg" alt="planet earth icon" />
-      <h1 className="header__title">Our travel journal.</h1>
+      <h1 className="header__title">Our travel journal 🐵🤍🐵</h1>
     </section>
   );
 }
